c93f09878a7bcb6f29c06be96527bd3b87c6fd2b
Otavio Leopoldino da Silva <1052200@senacminas.edu.br>
2026-02-23 20:16:16 -0300

    page initial

--- app/page.js
+++ app/page.js
@@ -2,16 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className=" h-100 w-100 flex items-center justify-around bg-zinc-200 rounded-2xl">
-        <div className="h-25 w-25 items-center justify-center bg-white text-black rounded-xl">
-          <button className="w-full h-full text-center text-black text-2xl border-2 border-zinc-300 shadow-xl/15">0</button>
-        </div>
-        <div className="h-50 w-50 flex flex-col items-center justify-center bg-transparent text-black gap-3">
-          <button className="w-full bg-white text-black text-xl rounded-lg border-2 border-zinc-300 shadow-xl/15">Incrementar</button>
-          <button className="w-full bg-white text-black text-xl rounded-lg border-2 border-zinc-300 shadow-xl/15">Zerar</button>
-          <button className="w-full bg-white text-black text-xl rounded-lg border-2 border-zinc-300 shadow-xl/15">Decrementar </button>
-        </div>
+    <div className="relative min-h-screen w-full">
+      <Image src="/fundolandpage.png" alt="FundoLand" fill className="object-cover opacity-30" priority/>
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4">
+        <h1 className="text-4xl md:text-6xl font-bold text-[#4988C4] mb-4">Qual tela você quer ir?</h1>
+      </div>
+      <div className="absolute bottom-60 left-0 right-0 flex justify-center items-center">
+        <a href="/contador" className="bg-[#4988C4] text-white text-2xl px-6 py-3 w-70 h-15 rounded-lg text-center flex justify-center items-center font-bold hover:bg-[#3a6ca0]">
+          Ir para o Contador
+        </a>
+        <a href="/calculadora" className="bg-[#4988C4] text-white text-2xl px-6 py-3 w-70 h-15 rounded-lg text-center flex justify-center items-center font-bold hover:bg-[#3a6ca0] ml-15">
+          Ir para a Calculadora
+        </a><a href="/idade" className="bg-[#4988C4] text-white text-2xl px-6 py-3 w-70 h-15 rounded-lg text-center flex justify-center items-center font-bold hover:bg-[#3a6ca0] ml-15">
+          Calcular Idade
+        </a>
       </div>
     </div>
   );
